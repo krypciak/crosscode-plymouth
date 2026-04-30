00931a0cdd4ab8c7f61f78148615a1de412b5c6a
krypek <krypek@tuta.io>
2026-04-30 20:07:02 +0200

    Do not regenerate initramfs by default in install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 DIR="$(printf "$(dirname $0)" | xargs realpath)"
 
-check_root () {
-  if [ ! $( id -u ) -eq 0 ]; then
+check_root() {
+  if [ ! $(id -u) -eq 0 ]; then
     echo -e "Must be run as root"
     exit 1
   fi
@@ -16,9 +16,8 @@ target_dir="/usr/share/plymouth/themes/crosscode"
 echo "copying theme to $target_dir"
 rm -rf "$target_dir"
 cp -r "$DIR" "$target_dir"
-echo "regenerating initramfs"
-plymouth-set-default-theme -R crosscode 
 
 if [ "${1-}" = 'preview' ]; then
+  plymouth-set-default-theme -R crosscode
   bash "$DIR"/preview.sh
 fi
